01f60369fa83a634b2fc219e481f1a2354fb5930
hanyusok <hanyusok@gmail.com>
2023-03-22 22:07:41 +0900

    time

--- src/stores/LeadStore.js
+++ src/stores/LeadStore.js
@@ -1,8 +1,11 @@
 import { defineStore } from "pinia"
+import { Timestamp } from 'firebase/firestore'
 
 export const useLeadStore = defineStore({
   id: "lead",
   state: () => ({
-    formData: {}
+    formData: {
+      timeStamp: Timestamp.now()
+    }
   })
 })
